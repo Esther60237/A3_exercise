6a041043df9a02cbb697897e84d4ecb22ba91a53
Esther <moutain60237@gmail.com>
2020-07-19 21:42:57 +0800

    set search routing

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
 
   res.render('index', { restaurants: restaurant.results })
 })
+
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurantInfo = restaurant.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurantInfo })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurantSearch = restaurant.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurantSearch, keyword: keyword })
 })
 
 app.listen(port, () => {
