89161c3d8b0fc3a507ccbd8d4604bb26851d15fd
Esther <moutain60237@gmail.com>
2020-08-01 22:46:40 +0800

    feat: add models

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,21 @@
+const Restaurant = require('../restaurant')
+const db = require('../../config/mongoose')
+const restaurant = require('../../restaurant.json')
+
+db.once('open', () => {
+  restaurant.results.forEach(restaurant => {
+    Restaurant.create({
+      name: `${restaurant.name}`,
+      name_en: `${restaurant.name_en}`,
+      category: `${restaurant.category}`,
+      image: `${restaurant.image}`,
+      location: `${restaurant.location}`,
+      phone: `${restaurant.phone}`,
+      google_map: `${restaurant.google_map}`,
+      rating: `${restaurant.rating}`,
+      description: `${restaurant.description}`
+    })
+  })
+  console.log('done')
+
+})
