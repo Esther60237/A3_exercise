e2e754c79d5950d07db3191b6520b857bfd959ac
Esther <moutain60237@gmail.com>
2020-08-01 22:53:20 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
-const exphbs = require('express-handlebars')
-const restaurant = require('./restaurant.json')
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-
-  res.render('index', { restaurants: restaurant.results })
-})
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurantInfo = restaurant.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
